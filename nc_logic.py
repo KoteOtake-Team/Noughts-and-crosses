9061cab5080d04261ea2ed08acdaa591e26bbfaa
become-iron <nikitaburkov@yandex.ru>
2015-04-21 18:21:50 +0300

    update nc_logic Нерабочая версия!

--- nc_logic.py
+++ nc_logic.py
@@ -6,52 +6,89 @@
 
 import nc_net
 
-field = []  # поле игры
-# Фигуры
-empty = ' '
-nought = 'o'
-cross = 'x'
-
-firstPlayer = ''  # фигура первого игрока
-secondPlayer = ''  # фигура второго игрока
-sequence = []  # хранение очередности ходов
-
-
-class FailedParty(Exception):
+class WrongData(Exception):
     """
-    Исключение, возникающее при провале создания партии
+    Исключение, возникающее при передаче функциям неверных типов данных
     """
     pass
 
 
-def start_game(**kwargs):
+def start_game(id=-1):
     """
     СОЗДАТЬ ПАРТИЮ
     Принимает:
-    ничего
-    Необязательные:
-    идентификатор игрока, с которым хотелось бы сыграть
-
+        id (int, необязательный, равен -1 по умолчанию) - идентификатор игрока, с которым хотелось бы сыграть
     Возвращает:
-    кортеж:
-    -очерёдность ходов
+    ###Возможно, лучще возвращаемые значения заменить на однозначно интерпретируемые коды###
+        tuple:
+            1) boolean - успешно ли создание партии
+                True - успешно
+                False - провал
+            2) boolean (только если создание партии успешно)- очередность ходов
+                True - текущий игрок ходит первым
+                False - текущий игрок ходит вторым
+            3) int (только если текущий игрок ходит вторым) - фигура противника
+                0 - крестики
+                1 - нолики
+            В случае отсутсвия возращаемого значения элемент заполняется None
+            Примеры:
+                (True, False, 0),
+                (True, True, None),
+                (False, None, None)
 
+    Если первым ходит текущий игрок, должна быть вызвана функция выбора фигуры choosing()
     """
-    value = nc_net.transfer()
-    x = 0
-    if value == True:
-        return x
-    else:
-        raise FailedParty
+    if type(id) != int:
+        raise WrongData
+
+    value = nc_net.transfer() if id == -1 else nc_net.transfer(id=id)  # обмен данными с сервером
+
+    # несогласованный пример. лишь для наглядности
+    # будут вложенные условия
+    if value == 1:
+        return False, None, None
+    elif value == 2:
+        return True, True, None
+    elif value == 3:
+        return True, False, 0
+    elif value == 4:
+        return True, False, 1
+
+def choosing(figure):
+    """
+    ВЫБОР ФИГУРЫ
+    Принимает:
+        int - идентификатор фигуры (крестики или нолик)
+            0 - крестик
+            1 - нолик
+    Возвращает:
+        boolean - успешен ли выбор фигуры
+            True - успешен
+            False - нет
+    """
+    if figure not in (0,1):
+        raise WrongData
+    value = nc_net.transfer()  # обмен данными с сервером
+    if value is True:
+        return True
+    elif value is False:
+        return False
 def moving(ceil):
     """
     ХОД ИГРОКА
     Принимает:
+        int - номер ячейки, выбранной игроком
+            0 - крестик
+            1 - нолик
+    Возвращает:
+        boolean - успешен ли выбор фигуры
+            True - успешен
+            False - нет
     """
-    pass
-def choosing():
-    """
-    ВЫБОР ФИГУРЫ
-
-    """
-    pass
+    if ceil not in (0, 1, 2, 3, 4, 5, 6, 7, 8):
+        raise WrongData
+    value = nc_net.transfer()
+    if value is True:
+        return True
+    elif value is False:
+        return False
