a9ed2efeac22deae96f148b641fd769282dd4312
Kirill Zvonov <kzvonov@gmail.com>
2015-05-18 12:11:30 +0300

    Update client
    1) request для передачи данных, формат dict
    2) get_response для ответа, формат dict
    // на данный момент, если все прикрутить, то можно сыграть в 2м(комната с игроками пока одна)

--- nc_net.py
+++ nc_net.py
@@ -45,9 +45,28 @@ def start():
         time.sleep(1)
 
 
+# testing methods for palaying 1 vs 1
+def request(data):
+    url = 'https://nc-server-fchaack.c9.io/req'
+    payload = json.dumps(data)
+    response = requests.post(url, headers=headers, data=payload)
+    return response
+    
+def get_response():
+    url = 'https://nc-server-fchaack.c9.io/res'
+    for i in range(900):
+        time.sleep(0.1)
+        response = requests.post(url, headers=headers)
+        if response:
+            return response.json()
+
 
 if __name__ == '__main__':
-    start()
-    print(key)
-    print()
+    #start()
+    #print(key)
+    #print()
+    
+    #cycle of data
+    request({'message':'Hello'})
+    print(get_response())
 
