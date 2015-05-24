e66db43ce2ea1a6985199a7d5c2b267d871606bc
Kirill Zvonov <kzvonov@gmail.com>
2015-05-24 21:28:42 +0300

    Almost ended client
    Примерно так будет выглядеть клиент.
    1) Соединяешься с сервером( connect_to_server() )
    2) Ищешь противника( get_enemy() )
    3) Отправляешь, получаешь данные( send_request, get_response )

--- nc_net.py
+++ nc_net.py
@@ -1,72 +1,80 @@
+# -*- coding: utf-8 -*-
 import time
 import requests, json
 
-url = 'https://nc-server-fchaack.c9.io'
+#  Настройки
+URL = 'https://nc-server-fchaack.c9.io'
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-my_key = ''
-enemy_key = ''
+def get_url():
+    return URL
 
 
-def simple_test(req_f):
-    def test(args, act=''):
-        print('action:', act)
-        print('request:',args)
-        return req_f(args)
-    return test
+# Данные о клиенте
+client_id = None
+battle_id = None
 
-@simple_test
-def send_request(data, act=''):
-    """
-    data must be dictionary
-    """
-    global url
-
-    url_with_act = url + act
-    print(url + act)
-    payload = json.dumps(data)
-    response = requests.post(url_with_act, headers=headers, data=payload)
-    return response.json()
+def get_data_about_client():
+    return {'id': client_id, 'battle': battle_id}
 
 
-def start():
-    global my_key,  enemy_key
-
-    connect = send_request({'message': 'I want to connect'})
-    if connect['key']:
-        my_key = connect['key']
-
-    while True:
-        get_enemy = send_request({'message': 'give me Enemy!'}, act='/search_enemy')
-        print(get_enemy)
-        if get_enemy:
-            enemy_key = get_enemy['enemy_key']
-            break
-        time.sleep(1)
-
-
-# testing methods for palaying 1 vs 1
-def request(data):
-    url = 'https://nc-server-fchaack.c9.io/req'
-    payload = json.dumps(data)
-    response = requests.post(url, headers=headers, data=payload)
-    return response
-    
-def get_response():
-    url = 'https://nc-server-fchaack.c9.io/res'
-    for i in range(900):
-        time.sleep(0.1)
+# Функции для передачи данныx
+def request(url, payload={}):
+    if payload:
+        payload = json.dumps(payload)
+        response = requests.post(url, headers=headers, data=payload)
+    else:
         response = requests.post(url, headers=headers)
-        if response:
-            return response.json()
+
+    return response.json
 
 
-if __name__ == '__main__':
-    #start()
-    #print(key)
-    #print()
-    
-    #cycle of data
-    request({'message':'Hello'})
-    print(get_response())
 
+def connect_to_server():
+    """
+    Соединяемся с сервером.
+    update -> client_id
+    """
+    url = get_url() + '/connect'
+    response = request(url, payload=get_data_about_client())
+    global client_id
+    client_id = response.json()['id']
+
+
+def get_enemy():
+    """
+    Отправляем серверу наши данные для поиска соперника.
+    update -> battle_id
+    """
+    url = get_url() + '/enemy'
+    data = get_data_about_client()
+    while True:
+        time.sleep(0.1)
+        response = request(url, payload=data)
+        if response: return response
+
+
+def send_request(data):
+    """
+    Отправить запрос серверу.
+    data = {key: value, ...}
+    return -> {'message': value}
+    """
+    url = get_url() + '/req'
+    response = request(url, payload=data.update(get_data_about_client())
+    return response
+
+
+def get_response(time=1):
+    """
+    time - время ожидания ответа в минутах
+    Получить ответ сервера.
+    return -> {'message': value, key: value, ...}
+    """
+    time *= 600
+    wait = 0.1
+    url = get_url() + '/res'
+    for i in range(time):
+        time.sleep(wait)
+        response = request(url)
+        if response: return response
